f0f34cc5811ded3af1db8353723016613ce45832
mtahafarooq <mtahafrq@gmail.com>
2023-01-10 16:41:13 +0100

    add addProduct action in basket slice

--- src/pages/basket/slices/basket-slice.ts
+++ src/pages/basket/slices/basket-slice.ts
@@ -1,5 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 import { Basket } from "../types/basket";
+import type { PayloadAction } from "@reduxjs/toolkit";
 
 export interface BasketStateModel {
   basket: Basket;
@@ -14,7 +15,16 @@ const REDUCER_NAME = "basket";
 export const basketSlice = createSlice({
   name: REDUCER_NAME,
   initialState,
-  reducers: {},
+  reducers: {
+    addProduct(state, action: PayloadAction<string>) {
+      if (state.basket[action.payload]) {
+        ++state.basket[action.payload];
+      } else {
+        state.basket[action.payload] = 1;
+      }
+    },
+  },
 });
+export const { addProduct } = basketSlice.actions;
 
 export default basketSlice.reducer;
