37b7bd7d9afc84a38cd2685e11d85dc868f828d0
mtahafarooq <mtahafrq@gmail.com>
2023-01-10 16:13:06 +0100

    render rules ui in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Products from "./pages/products/containers/product-container";
+import Rules from "./pages/rules/containers/rules-container";
 
 export default function App() {
   return (
     <div>
       <Products />
+      <Rules />
     </div>
   );
 }
